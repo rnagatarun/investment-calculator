a6c411d4bf99819baa28bf9e50395eb349dc5a5c
NagaTarun <rnagatarun@gmail.com>
2024-12-18 06:30:38 +0530

    initialize project and add Header component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+import Header from './components/Header.tsx'
+
+function App() {
+
+  return (
+    <>
+     <Header />
+    </>
+  )
+}
+
+export default App
